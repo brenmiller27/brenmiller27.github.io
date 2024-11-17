20b2455151381ebdef8941dd689904c5eb0012d7
brenmiller27 <167664968+brenmiller27@users.noreply.github.com>
2024-11-17 11:26:05 -0800

    initial commit

--- scripts/temples.js
+++ scripts/temples.js
@@ -4,13 +4,13 @@ const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
 hamburger.addEventListener("click", ()  => {
-    hamburger.classlist.toggle("active");
-    navMenu.classlist.toggle("active");
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
 })
 
 document.querySelectorAll('.nav-link').forEach(n => n.
 addEventListener("click", () => {
-      hamburger.classlist.remove("active");
-      navMenu.classlist.remove("active");
+      hamburger.classList.remove("active");
+      navMenu.classList.remove("active");
     
 }))
