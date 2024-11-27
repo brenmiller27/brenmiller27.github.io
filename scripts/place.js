13284fe86a936809c12853e6b2d28d6fbe231579
brenmiller27 <167664968+brenmiller27@users.noreply.github.com>
2024-11-26 16:43:42 -0800

    initial commit

--- scripts/place.js
+++ scripts/place.js
@@ -1,3 +1,14 @@
-function calculateWindchill(){
-    let
+function calculateWindchill() {
+
+    var  temp = 72;
+    var speed= 15
+    var windChill = (35.74 + (0.6215 * temp))-
+    (35.75 * Math.pow(speed, 0.16));
+
+    var windChill = Math.round(windChill);
+    document.getElementById('windChill').innerHTML = windChill;
+
 }
+
+calculateWindchill();
+
