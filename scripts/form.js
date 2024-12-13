cd1bb859b4f2e3a7ba08173db2ed7034113f3b8c
brenmiller27 <167664968+brenmiller27@users.noreply.github.com>
2024-12-12 19:41:13 -0800

    initial commit

--- scripts/form.js
+++ scripts/form.js
@@ -1,11 +1,31 @@
 document. addEventListener("DOMContentLoaded" , function () {
     const productArray = [
-        {id: 1, name: "Product 1"},
-        {id: 2, name: "Product 2"},
-        {id: 3, name: "Product 3"},
-        {id: 4, name: "Product 4"},
-
-    ];
+        {
+          id: "fc-1888",
+          name: "flux capacitor",
+          averagerating: 4.5
+        },
+        {
+          id: "fc-2050",
+          name: "power laces",
+          averagerating: 4.7
+        },
+        {
+          id: "fs-1987",
+          name: "time circuits",
+          averagerating: 3.5
+        },
+        {
+          id: "ac-2000",
+          name: "low voltage reactor",
+          averagerating: 3.9
+        },
+        {
+          id: "jj-1969",
+          name: "warp equalizer",
+          averagerating: 5.0
+        }
+      ];
 
     const productSelect =  document.getElementById('productName');
 
