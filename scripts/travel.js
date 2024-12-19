547e6ab3afd4ce5bf7617e93cbd72209ddf65d17
brenmiller27 <167664968+brenmiller27@users.noreply.github.com>
2024-12-18 19:28:21 -0800

    initial commit

--- scripts/travel.js
+++ scripts/travel.js
@@ -7,13 +7,71 @@ const navMenu = document.querySelector(".nav-menu");
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
-})
+});
 
 document.querySelectorAll('.nav-link').forEach(n => n.
 addEventListener("click", () => {
-    hamburger.classList.remove("active")
-    navMenu.classList.remove("active")
-}))
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}));
 
-window.addEventListener('scroll', blurHeader)
 
+
+window.addEventListener('scroll', blurHeader);
+
+const scrollup = () => {
+   
+    const scrollup = document.getElementById('scrollup') 
+    this.scrollY >= 350 ? scrollup.classList.add('show-scroll')
+                        : scrollup.classList.remove('show-scroll')
+}
+
+window.addEventListener('scroll', scrollup);
+
+
+document. addEventListener("DOMContentLoaded" , function () {
+    const productArray = [
+        {
+          id: "fc-1888",
+          name: "flux capacitor",
+          averagerating: 4.5
+        },
+        {
+          id: "fc-2050",
+          name: "power laces",
+          averagerating: 4.7
+        },
+        {
+          id: "fs-1987",
+          name: "time circuits",
+          averagerating: 3.5
+        },
+        {
+          id: "ac-2000",
+          name: "low voltage reactor",
+          averagerating: 3.9
+        },
+        {
+          id: "jj-1969",
+          name: "warp equalizer",
+          averagerating: 5.0
+        }
+      ];
+
+    const productSelect =  document.getElementById('productName');
+
+    productArray.forEach ((product) => {
+        let option = document.createElement("option");
+        option.value = product.id;
+        option.textContent = product.name ;
+        productSelect.appendChild(option);
+
+    });
+});
+
+if (window.location.pathname.includes('review.html')) {
+    let reviewCount = localStorage.getItem('reviewCount');
+    reviewCount = reviewCount ? parseInt(reviewCount) + 1 : 1;
+    localStorage.setItem('reviewCount', reviewCount);
+    document.body.innerHTML = `<h1>Thank you for your review!</h1><p>You have submitted ${reviewCount} reviews.</p>`;
+}
