36125d4480322d7f08e126f4e35db69e193ef3e4
brenmiller27 <167664968+brenmiller27@users.noreply.github.com>
2024-12-18 20:51:51 -0800

    initial commit

--- scripts/form.js
+++ scripts/form.js
@@ -2,27 +2,27 @@ document. addEventListener("DOMContentLoaded" , function () {
     const productArray = [
         {
           id: "fc-1888",
-          name: "flux capacitor",
+          name: "Site Plan",
           averagerating: 4.5
         },
         {
           id: "fc-2050",
-          name: "power laces",
+          name: "Travel Website",
           averagerating: 4.7
         },
         {
           id: "fs-1987",
-          name: "time circuits",
+          name: "Trip plan",
           averagerating: 3.5
         },
         {
           id: "ac-2000",
-          name: "low voltage reactor",
+          name: "Accesibility",
           averagerating: 3.9
         },
         {
           id: "jj-1969",
-          name: "warp equalizer",
+          name: "Guides",
           averagerating: 5.0
         }
       ];
@@ -44,3 +44,20 @@ if (window.location.pathname.includes('review.html')) {
     localStorage.setItem('reviewCount', reviewCount);
     document.body.innerHTML = `<h1>Thank you for your review!</h1><p>You have submitted ${reviewCount} reviews.</p>`;
 }
+
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll('.nav-link').forEach(n => n.
+addEventListener("click", () => {
+    hamburger.classList.remove("active")
+    navMenu.classList.remove("active")
+}));
+
